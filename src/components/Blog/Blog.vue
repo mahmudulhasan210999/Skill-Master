b83b2d18c755dc8b7c77a4f6e01a694d46f7657b
Siam05 <siam.mahmud05@gmail.com>
2022-05-18 14:20:37 +0600

    blog page style

--- src/components/Blog/Blog.vue
+++ src/components/Blog/Blog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">
+  <div class="w-full flex flex-col bg-gray-50 px-44">
     <div class="text-center p-12">
       <p class="text-5xl font-bold p-4">New & Top Articles</p>
       <div class="flex justify-center">
@@ -9,13 +9,13 @@
       </div>
     </div>
 
-    <div class="grid grid-cols-3 px-40">
-      <div class="p-3 " v-for="(blog, index) in blogs" :key="index">
-        <div class="p-5">
+    <div class="grid grid-cols-3">
+      <div class="p-4" v-for="(blog, index) in blogs" :key="index">
+        <div>
           <div>
-            <router-link :to="{ path: '/blog/' + blog.slug, params: { id: blog.title }}"><img class="" :src="blog.img" alt="Image"></router-link>
+            <router-link :to="{ path: '/blog/' + blog.slug, params: { id: blog.title }}"><img class="rounded-t-lg" :src="blog.img" alt="Image"></router-link>
           </div>
-          <div class="p-4 border border-gray-200">
+          <div class="p-4 bg-white rounded-b-lg">
             <p class="text-xl font-medium">{{ blog.title }}</p>
             <div class="flex pt-2">
               <img class="rounded-full h-8 w-8" :src="blog.writer_img" alt="Image">
@@ -26,8 +26,8 @@
       </div>
     </div>
 
-    <div class="flex justify-center p-10">
-      <button class="border border-gray-500 rounded-md hover:bg-red-500 p-4">Load More ⟳</button>
+    <div class="flex justify-center p-4">
+      <button class="border border-gray-400 hover:border-red-500 rounded-md hover:text-white hover:bg-red-500 px-6 py-3">Load More ⟳</button>
     </div>
   </div>
 </template>
