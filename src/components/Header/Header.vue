e6ebed09a558e19d2282537100cbefadbb80c24c
Siam05 <siam.mahmud05@gmail.com>
2022-05-25 17:46:45 +0600

    header mobile-view

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,45 +1,43 @@
 <template>
     <div class="w-full">
         <!-- Main Header -->
-        <div class="hidden w-full md:flex justify-between px-36 shadow-lg navbar">
+        <div class="hidden w-full lg:flex lg:justify-between lg:px-16 xl:px-36 shadow-lg">
             <div class="flex items-center">
-                <div class="mr-20">
+                <div class="mr-12 xl:mr-20">
                     <router-link to="/">
-                        <img class="h-20" src="../../assets/logo/platform-logo.png" alt="">
+                        <img class="h-20" src="../../assets/logo/platform-logo.png" alt="logo">
                     </router-link>
                 </div>
                 <div v-for="(item, index) in menu" :key="index">
                     <router-link :to="{ path: item.path}">
-                        <p class="mx-2 px-4 py-2 my-2 hover:text-rose-600">{{item.title}}</p>
+                        <p class="mx-2 px-2 xl:px-4 py-2 my-2 hover:text-rose-600">{{item.title}}</p>
                     </router-link>
                 </div>
             </div>
             <div class="flex items-center">
                 <router-link to="/cart">
-                    <div class="flex items-center mr-16 border px-5 py-2 rounded-md hover:border-2 hover:border-rose-600 hover:text-rose-700">
+                    <div class="flex items-center mr-8 xl:mr-16 border px-3 xl:px-5 py-2 rounded-md hover:border-2 hover:border-rose-600 hover:text-rose-700">
                         <i class="pi pi-shopping-cart"></i>
                         <p class="ml-2">Cart</p>
                     </div>
                 </router-link>
                 <router-link to="/login">
-                    <button class="py-2 px-5 text-rose-600 border rounded-md font-semibold hover:bg-rose-600 hover:text-white">Sign in</button>
+                    <button class="py-2 px-3 xl:px-5 text-rose-600 border rounded-md font-semibold hover:bg-rose-600 hover:text-white">Sign in</button>
                 </router-link>
                 <router-link to="/registration">
-                    <button class="py-2 px-5 border rounded-md bg-black font-semibold text-white ml-4">Sign up</button>
+                    <button class="py-2 px-3 xl:px-5 border rounded-md bg-black font-semibold text-white ml-2 xl:ml-4">Sign up</button>
                 </router-link>
             </div>
         </div>
 
         <!-- Header For Responsive Dimensions -->
-        <div class="flex justify-between md:hidden">
-            <div class="pl-4">
-                <router-link class="text-lg font-medium" :to="'/'"><img class="h-12 w-44" src="../../assets/logo/platform-logo.png" alt="HOME"></router-link>
-            </div>
-            <div class="pr-5 pt-3">
-                <div class="">
-                    <i @click="toggleNav" class="pi pi-bars p-1"></i>
-                    <Navbar v-if="navToggled" @navClosed="closeNav"/>
-                </div>
+        <div class="flex items-center justify-between px-6 sm:px-12 lg:hidden">
+            <router-link class="text-lg font-medium" :to="'/'">
+                <img class="h-16 w-44" src="../../assets/logo/platform-logo.png" alt="logo">
+            </router-link>
+            <div>
+                <i @click="toggleNav" class="pi pi-bars p-1"></i>
+                <Navbar v-if="navToggled" @navClosed="closeNav"/>
             </div>
         </div>
     </div>
@@ -78,10 +76,4 @@ export default {
 </script>
 
 <style scoped>
-.navbar {
-  overflow: hidden;
-  z-index: 100;
-  top: 0;
-  width: 60%;
-}
 </style>
--- src/components/Header/nav.vue
+++ src/components/Header/nav.vue
@@ -1,65 +1,87 @@
 <template>
-  <div class="flex flex-col navbar">
-    <div class="nav-cross">
-      <i @click="$emit('navClosed')" class="pi pi-times py-2.5 mx-4 px-3"></i>
-    </div>
-
-    <div class="p-2">
-      <div class="">
-        <div v-for="(item, index) in menu" :key="index">
-          <router-link :to="{ path: item.path}">
-            <p class="mx-2 px-4 py-2 my-2 text-white">{{item.title}}</p>
-          </router-link>
+    <div class="flex flex-col navbar">
+        <div class="py-3 nav-cross">
+            <router-link to="/cart">
+                <div class="flex items-center text-white border border-white px-5 py-2 rounded-md hover:border-2 hover:border-rose-600 hover:text-rose-700">
+                    <i class="pi pi-shopping-cart"></i>
+                    <p class="ml-2">Cart</p>
+                </div>
+            </router-link>
+            <div class="flex items-center text-white border border-white mx-4 px-3 py-3 rounded-md hover:border-2 hover:border-rose-600 hover:text-rose-700">
+                <i @click="$emit('navClosed')" class="pi pi-times"></i>
+            </div>
         </div>
-      </div>
 
-      <div class="flex p-4">
-        <router-link to="/login">
-          <button class="py-2 px-5 text-rose-600 border rounded-md font-semibold bg-white">Sign in</button>
-        </router-link>
-        <router-link to="/registration">
-          <button class="py-2 px-5 border rounded-md bg-black font-semibold text-white ml-4">Sign up</button>
-        </router-link>
-      </div>
+        <div> 
+            <div v-for="(item, index) in menu" :key="index">
+                <router-link :to="{ path: item.path}">
+                    <p class="mx-2 px-4 py-2 my-2 text-white hover:text-rose-600">{{item.title}}</p>
+                </router-link>
+            </div>
+
+            <div class="flex p-4">
+                <router-link to="/login">
+                    <button class="py-2 px-3 sm:px-5 text-rose-600 border rounded-md font-semibold bg-white">Sign in</button>
+                </router-link>
+                <router-link to="/registration">
+                    <button class="py-2 px-3 sm:px-5 border rounded-md bg-black font-semibold text-white ml-2 sm:ml-4">Sign up</button>
+                </router-link>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 
 export default {
-  computed: {
-    ...mapState ({
-      menu: state => state.menu.menu
-    }),
-  }
+    computed: {
+        ...mapState ({
+            menu: state => state.menu.menu
+        }),
+    }
 }
 </script>
 
 <style scoped>
 .navbar {
-  background: #1C2138;
-  z-index: 999;
-  width: 75vw;
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+    background: #1C2138;
+    z-index: 999;
+    width: 70vw;
+    position: fixed;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
 }
+
 .nav-cross { 
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: flex-end;
-  align-self: flex-start;
-  padding-top: 3%;
-  padding-right: 0%;
-  margin-bottom: 4%;
-  border-bottom-width: 2px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    align-self: flex-start;
+    border-bottom-width: 2px;
+}
+
+@media screen and (max-width: 1024px) {
+    .navbar {
+        width: 50vw;
+    }
+}
+
+@media screen and (max-width: 850px) {
+    .navbar {
+        width: 55vw;
+    }
+}
+
+@media screen and (max-width: 540px) {
+    .navbar {
+        width: 70vw;
+    }
 }
 </style>
