f768ba9424cfa73963ba43847d123f2dfb68b070
mahmudulhasan210999 <mahmudulhasan210999@gmail.com>
2022-05-31 16:50:24 +0600

    Recent blogs home page

--- src/components/Home/Recent_Articles.vue
+++ src/components/Home/Recent_Articles.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="w-full my-12 px-8 xl:px-44">
-    <p class="text-lg lg:text-xl font-semibold p-2">Recent Articles</p>
+    <p class="text-lg lg:text-xl font-semibold p-2">Recent Blogs</p>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-      <div class="xl:p-3 " v-for="(article, index) in articles" :key="index">
+      <div class="xl:p-3 " v-for="(blog, index) in blogs" :key="index">
         <div class="p-3 lg:p-4">
           <div>
-            <img class="" :src="article.img" alt="Image">
+            <img class="h-60 md:h-52 xl:h-60 w-full" :src="blog.headerImage" alt="Image">
           </div>
           <div class="p-1 lg:p-3 border border-gray-200">
-            <p class="lg:text-xl font-medium p-1 lg:p-2">{{ article.title }}</p>
+            <p class="lg:text-xl font-medium p-1 lg:p-2">{{ blog.title }}</p>
             <div class="flex px-1 lg:px-2">
-              <img class="rounded-full h-9 w-9" :src="article.writer_img" alt="Image">
-              <p class="p-2 text-sm text-gray-600">{{ article.writer }}</p>
+              <img class="rounded-full h-9 w-9" :src="blog.author_thumb" alt="Image">
+              <p class="p-2 text-sm text-gray-600">{{ blog.author_name }}</p>
             </div>
           </div>
         </div>
@@ -30,8 +30,13 @@ export default {
   },
   computed: {
     ...mapState ({
-      articles: state => state.home.articles
+      // articles: state => state.home.articles,
+      blogs: state => state.blogs.recent_blogs,
     }),
+  },
+
+  mounted() {
+    this.$store.dispatch('blogs/getRecentBlogs')
   }
 }
 </script>
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -135,30 +135,6 @@ const state = {
       lessos: '10'
     },
   ],
-
-  articles: [
-    {
-      id: 1,
-      img: image,
-      title: 'The Complete Business Course',
-      writer_img: image,
-      writer: 'Robert',
-    },
-    {
-      id: 2,
-      img: image,
-      title: 'The Complete Mobile Course',
-      writer_img: image,
-      writer: 'Maxx',
-    },
-    {
-      id: 3,
-      img: image,
-      title: 'The Complete Burger Course',
-      writer_img: image,
-      writer: 'Robert',
-    },
-  ],
 }
 
 const getters = {
